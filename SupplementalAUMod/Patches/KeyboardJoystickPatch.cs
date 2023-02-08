787886cf3ad4f6bc726819385a52e32d30bb6c1f
Hyz-sui <86903430+Hyz-sui@users.noreply.github.com>
2023-02-08 18:30:21 +0900

    move preprocessor directive

--- SupplementalAUMod/Patches/KeyboardJoystickPatch.cs
+++ SupplementalAUMod/Patches/KeyboardJoystickPatch.cs
@@ -4,17 +4,17 @@ using InnerNet;
 
 namespace AUMod.Patches
 {
+#if DEBUG
     [HarmonyPatch(typeof(KeyboardJoystick), nameof(KeyboardJoystick.Update))]
     public static class KeyboardJoystickUpdatePatch {
         public static void Postfix()
         {
-#if DEBUG
             if (!AmongUsClient.Instance.AmHost || AmongUsClient.Instance.GameState != InnerNetClient.GameStates.Started) return;
 
             if ((Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl)) && Input.GetKeyDown(KeyCode.F5)) {
                 GameManager.Instance.RpcEndGame(GameOverReason.ImpostorByKill, false);
             }
-#endif
         }
     }
+#endif
 }
